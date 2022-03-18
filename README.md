21ddca9ff9e2bf4a72be96aeb5137a329ea9f4a3
Mate Kovacs <mate@braid.tech>
2022-03-18 19:20:28 +0900

    add small example demonstrating the problem

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # serde-json-bug
-Reproducing a bug in Rust's serde_json crate
+
+This snippet reproduces a bug in Rust's `serde_json` crate.
+
+Expected output:
+```
+Err(Error("unknown variant `xenomorph`, expected `cat` or `dog`", line: 6, column: 34))
+```
+
+Observed output:
+```
+Err(Error("unknown variant `xenomorph`, expected `cat` or `dog`", line: 0, column: 0))
+```
---
+++ src/main.rs
@@ -0,0 +1,37 @@
+const JSON_TEXT: &str = r#"
+{
+    "things_tag": "animals",
+    "items": [
+        {"animal_tag": "cat"},
+        {"animal_tag": "xenomorph"},
+        {"animal_tag": "dog"}
+    ]
+}
+"#;
+
+fn main() {
+    let stuff = serde_json::from_str::<Things>(JSON_TEXT);
+    println!("{:?}", stuff);
+}
+
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+#[serde(tag = "things_tag", rename_all = "snake_case")]
+enum Things {
+    Animals(Animals),
+    Plants(Plants),
+}
+
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+struct Animals {
+    items: Vec<Animal>,
+}
+
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+#[serde(tag = "animal_tag", rename_all = "snake_case")]
+enum Animal {
+    Cat,
+    Dog,
+}
+
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+struct Plants {}
